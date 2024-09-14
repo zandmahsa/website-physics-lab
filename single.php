1bde4b8126b1347d3d3683e08c78427095afae49
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:21:26 -1200

    Update single.php

--- single.php
+++ single.php
@@ -1,32 +1,22 @@
 <?php get_header(); ?>
- 
-   <?php      
-            while(have_posts())
-            {
-                the_post();
-				?>
- <section>
+
+<?php      
+while (have_posts()) {
+    the_post();
+?>
+    <section>
         <div class="post">
-<br>		
-			 <h1 ><big><?php the_title();?></big></h1>
-			 <br>
-			 <br>
-            <div >
-			<br>
-			<br>
-			<br>
-               <?php the_content(); ?>
+            <br>
+            <h1><big><?php the_title(); ?></big></h1>
+            <br><br>
+            <div>
+                <br><br><br>
+                <?php the_content(); ?>
             </div>
         </div>
-		
     </section>
-	<?php
-	}
-	?>
-	 
- <?php get_footer(); ?>
+<?php
+}
+?>
 
-
-
-
-
+<?php get_footer(); ?>
