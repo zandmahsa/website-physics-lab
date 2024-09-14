2de6a22f7480927dcfe8c7cbfb6a210672658903
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:23:43 -1200

    Update page.php

--- page.php
+++ page.php
@@ -1,32 +1,25 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // اینجا که حلقه قرار دادیم ?>
 
-<?php if (has_post_thumbnail()){ // اینجا هم که تصویر شاخص رو میزاریم نشون بده
- the_post_thumbnail('news');
- }
- ?>            
+<?php if (have_posts()) : while (have_posts()) : the_post(); // حلقه برای نمایش پست‌ها ?>
+
+    <?php if (has_post_thumbnail()) { // نمایش تصویر شاخص در صورت وجود
+        the_post_thumbnail('news');
+    } ?>
    
- <section>
-		
+    <section>
         <div class="post"> 
-		<br >
-			 <h1 ><big><?php the_title();?></big></h1>
-			 <br>
-			 <br>
-            <div >
-			<br>
-			<br>
-			<br>
-               <?php the_content(); ?>
+            <br>
+            <h1><big><?php the_title(); ?></big></h1>
+            <br><br>
+            <div>
+                <br><br><br>
+                <?php the_content(); ?>
             </div>
         </div>
-		
     </section>
-	
-	<?php endwhile; else : ?>
- <p><?php _e('چیزی پیدا نشد','zand_physics'); ?></p>
+
+<?php endwhile; else : ?>
+    <p><?php _e('چیزی پیدا نشد', 'zand_physics'); ?></p>
 <?php endif; ?>
-  </div>
-	 
 
 <?php get_footer(); ?>
