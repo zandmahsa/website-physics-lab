339e5a6a2e946c489c81909bd7e2e16c86439bcd
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:17:51 -1200

    Update newspage.php

--- inc/newspage.php
+++ inc/newspage.php
@@ -5,28 +5,23 @@ Template Name: newsform
 get_header();
 ?>
 
-<div style="height:650px;overflow:scroll;width:100%;">
-            <header>
-                <div>
-                    <div>
-                        <hr id="line">
-                        <h1 id="titr"><?php _e('اخبار و اطلاعیه ها','zand_physics'); ?></h1>
-                        <hr id="line">
-                    </div>
-                </div>
-            </header>
-            <div >
-                <ul class="news">
-                
-				<li style="display: flex;flex-wrap: wrap;">
-	<?php dynamic_sidebar( 'news' ); ?></li>
-	
-	
-                </ul>
-				
-               
-                 
+<div style="height:650px; overflow:scroll; width:100%;">
+    <header>
+        <div>
+            <div>
+                <hr id="line">
+                <h1 id="titr"><?php _e('اخبار و اطلاعیه ها', 'zand_physics'); ?></h1>
+                <hr id="line">
             </div>
-            
+        </div>
+    </header>
+    <div>
+        <ul class="news">
+            <li style="display: flex; flex-wrap: wrap;">
+                <?php dynamic_sidebar('news'); ?>
+            </li>
+        </ul>
+    </div>
 </div>
-             <?php get_footer(); ?>
+
+<?php get_footer(); ?>
