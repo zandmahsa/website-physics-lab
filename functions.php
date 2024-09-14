6aac04b5c695e53201384c8bbf918b3b48e447c7
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:25:08 -1200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,53 +1,36 @@
 <?php
-/*add_action('plugins_loaded', 'wan_load_textdomain');
-function wan_load_textdomain() {
-	load_plugin_textdomain( 'shakhes', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
-}
-*/
+
 function register_my_menus() {
-  register_nav_menus(
-    array(
-      'main-menu' => __( 'منوی سایت' ),
-      'login-menu' => __( 'منوی ورود' ),
-	  'footer-menu' => __( 'پیوندهای سایت' )
-      
-    )
-  );
+    register_nav_menus(
+        array(
+            'main-menu'   => __( 'منوی سایت' ),
+            'login-menu'  => __( 'منوی ورود' ),
+            'footer-menu' => __( 'پیوندهای سایت' )
+        )
+    );
 }
-add_action( 'init', 'register_my_menus' );
+add_action('init', 'register_my_menus');
 
-
-
-if ( function_exists( 'register_sidebar' ) ) {
-
-    register_sidebar( array(
-        'name'         =>"اخبار و اطلاعیه ها",
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+        'name'         => "اخبار و اطلاعیه ها",
         'id'           => 'qomuninews',
-        'description'  => " اخبار و اطلاعیه ها"
-    ) );
-    register_sidebar( array(
-        'name'         =>"2اخبار و اطلاعیه ها",
+        'description'  => "اخبار و اطلاعیه ها"
+    ));
+    register_sidebar(array(
+        'name'         => "2اخبار و اطلاعیه ها",
         'id'           => 'qomuninews2',
-        'description'  => " اخبار و اطلاعیه ها"
-    ) );
-	register_sidebar( array(
-        'name'         =>"خبر",
+        'description'  => "اخبار و اطلاعیه ها"
+    ));
+    register_sidebar(array(
+        'name'         => "خبر",
         'id'           => 'news',
         'description'  => "خبر"
-    ) );
-	
-	
+    ));
 }
-
-
-
-
-
-
 
 function excerpt_readmore($more) {
-        return '... ' . 'Read More' . '';
+    return '... ' . 'Read More' . '';
 }
 add_filter('excerpt_more', 'excerpt_readmore');
-
- ?>
+?>
