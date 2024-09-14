cfd4c1ed37122488a17e06a72464284f6c2d0193
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:23:15 -1200

    Update search.php

--- search.php
+++ search.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
-    
-            
-           
-            
-<?php if(have_posts()) : ?> <?php while(have_posts()) : the_post(); ?>
-<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-<?php the_excerpt(); ?>
-<?php endwhile;else : ?>
- <h1 id="titr" style="margin-bottom: 10px;"><big><?php _e('چیزی پیدا نشد','zand_physics'); ?></big></h1>
-<?php endif; ?> 
-            
-			
-    <?php get_footer(); ?>
+
+<?php if (have_posts()) : ?> 
+    <?php while (have_posts()) : the_post(); ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php the_excerpt(); ?>
+    <?php endwhile; ?>
+<?php else : ?>
+    <h1 id="titr" style="margin-bottom: 10px;"><big><?php _e('چیزی پیدا نشد', 'zand_physics'); ?></big></h1>
+<?php endif; ?>
+
+<?php get_footer(); ?>
