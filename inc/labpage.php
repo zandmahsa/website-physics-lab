78bfff790969ea57a29099ddfa0ef39586f14c8c
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2024-09-14 06:16:39 -1200

    Update labpage.php

--- inc/labpage.php
+++ inc/labpage.php
@@ -5,40 +5,36 @@ Template Name: lab
 get_header();
 ?>
 
-<div style="height:650px;overflow:scroll;width:100%;">
-           <header>
-                <div>
-
-                    <hr id="line">
-                    <h1 id="titr"><big><?php _e('خدمات آزمایشگاهی','zand_physics'); ?></big></h1>
-                    <hr id="line">
-                </div>
-            </header>
-            <div >
-               <ul class="em">
-				<?php
-				$q=new WP_Query(
-					array("category_name"=>"آزمایشگاه","posts_per_page"=>10000)
-				);
-				while($q->have_posts())
-				{
-					$q->the_post();
-				?>
-					<li>
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-						</a>
-					</li>
-				<?php
-				}	
-
-				?>
-                     
-            </ul>
-				
-               
-                 
-            </div>
-            
+<div style="height:650px; overflow:scroll; width:100%;">
+    <header>
+        <div>
+            <hr id="line">
+            <h1 id="titr"><big><?php _e('خدمات آزمایشگاهی', 'zand_physics'); ?></big></h1>
+            <hr id="line">
+        </div>
+    </header>
+    <div>
+        <ul class="em">
+            <?php
+            $q = new WP_Query(
+                array(
+                    "category_name" => "آزمایشگاه",
+                    "posts_per_page" => 10000
+                )
+            );
+            while ($q->have_posts()) {
+                $q->the_post();
+            ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+    </div>
 </div>
-             <?php get_footer(); ?>
+
+<?php get_footer(); ?>
